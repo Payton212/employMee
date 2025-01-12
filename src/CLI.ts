0b796d22b3c1e5cff1db5dacbfa1b0147436b06b
payton <payton@paytons-Air.lan>
2025-01-11 22:58:17 -0700

    added cli class and started implementing first inquiry

---
+++ src/CLI.ts
@@ -0,0 +1,58 @@
+// importing classes from other files
+import inquirer from "inquirer";
+import department from "./departments.js";
+import role from "./roles.js";
+import employee from "./employees.js";
+
+class Cli {
+    constructor() { }
+    
+    originChoice(): void{
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "firstAction",
+                message: "what would you like to do?",
+                choices: ["View All Employees","Add Employees", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"],
+            },
+        ])
+            .then((answers) => {
+                if (answers.firstAction === "View All Employees") {
+                    this.ViewAllEmployees();
+                } else if (answers.firstAction === "Add Employees") {
+                    this.AddEmployee();
+                } else if (answers.firstAction === "View All Roles") {
+                    this.ViewRoles();
+                } else if (answers.firstAction === "Add Role") {
+                    this.AddRole();
+                } else if (answers.firstAction === "View All Departments") {
+                    this.ViewDepartments();
+                } else if (answers.firstAction === "Add Department") {
+                    this.AddDepartment();
+                } else if (answers.firstAction === "Quit") {
+                    this.Quit();
+            }
+        })
+    }
+    ViewAllEmployees() {
+        throw new Error("Method not implemented.");
+    }
+    AddEmployee() {
+        throw new Error("Method not implemented.");
+    }
+    ViewRoles() {
+        throw new Error("Method not implemented.");
+    }
+    AddRole() {
+        throw new Error("Method not implemented.");
+    }
+    ViewDepartments() {
+        throw new Error("Method not implemented.");
+    }
+    AddDepartment() {
+        throw new Error("Method not implemented.");
+    }
+    Quit() {
+        throw new Error("Method not implemented.");
+    }
+}
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-// importing classes from other files
-import inquirer from "inquirer";
-import department from "./departments.js";
-import role from "./roles.js";
-import employee from employees
-
